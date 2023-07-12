a75a1b5359a2491440520d8ae3a159a8faa8f44b
pltnkvart <artosi@yandex.ru>
2023-07-12 14:14:58 +0300

    added gitignore and table_all.js

---
+++ pages/scripts/table_all.js
@@ -0,0 +1,12 @@
+var table = document.querySelector("table");
+var trs = table.getElementsByTagName("tr");
+var tds = null;
+
+user_tds = trs[1].getElementsByTagName("td");
+user_id = user_tds[0];
+crit_1 = user_tds[1];
+crit_2 = user_tds[2];
+crit_3 = user_tds[3];
+crit_4 = user_tds[4];
+crit_5 = user_tds[5];
+crit_sum = user_tds[6];
