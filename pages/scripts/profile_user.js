74d5e1a7f2a74291165ed421f07861bf76326137
pltnkvart <artosi@yandex.ru>
2023-07-12 18:36:16 +0300

    profile_user.js

---
+++ pages/scripts/profile_user.js
@@ -0,0 +1,16 @@
+user_id = document.getElementById("user_id");
+
+rating_number = document.getElementById("rating_number");
+
+sum_crit = document.getElementById("sum_crit");
+
+crit_1 = document.getElementById("crit_1");
+crit_2 = document.getElementById("crit_2");
+crit_3 = document.getElementById("crit_3");
+crit_4 = document.getElementById("crit_4");
+crit_5 = document.getElementById("crit_5");
+
+
+//id="file_input_person" - для вставки и отправки файла подтверждения
+
+//+ нужно менять стиль у линии прогресса (там width - но это потом если нужно будет)
